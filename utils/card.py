1f9df48438533096c5d7e849ba9d2af6055d74a5
drewburr <drewburr9@gmail.com>
2022-05-26 21:14:16 -0400

    Add the ability to add and copy cards

--- utils/card.py
+++ utils/card.py
@@ -1,7 +1,32 @@
 class Card():
     def __init__(self, attributes: "list[int]"):
-        self.attributes = attributes
+        self.__attributes = attributes
         print(attributes)
 
-    def __add__():
-        pass
+    def __add__(self, obj):
+        """
+        Add attribues of another card of the same attribute size.
+        Can add bare attributes list, if desired.
+        """
+        result: "list[int]" = []
+        objValues: "list[int]" = None
+        if type(obj) == type(self):
+            # obj is card
+            objValues = obj.__attributes
+        elif type(obj) == type(result):
+            # obj is attributes list
+            objValues = obj
+        else:
+            raise TypeError(
+                "Unable to add type Card to type: " + str(type(obj)))
+
+        if len(self.__attributes) != len(objValues):
+            raise ValueError(
+                f"Unable to add Card with different attributes. Got {str(len(objValues))}, expected {str(len(self.__attributes))}")
+
+        for a, b in zip(self.__attributes, objValues):
+            result.append(a+b)
+        return result
+
+    def __copy__(self):
+        return Card(self.__attributes)
